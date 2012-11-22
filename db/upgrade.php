74aa3e87a49ccbb958bfa47e1a28fd781df98cdd
Mike Churchward <mike@remote-learner.net>
2012-11-22 15:30:53 -0500

    CONTRIB-3937 - Add updates for testing new features.

--- db/upgrade.php
+++ db/upgrade.php
@@ -13,7 +13,7 @@ function xmldb_questionnaire_upgrade($oldversion) {
     }
 
     // v2.3.0 release upgrade line
-    if ($oldversion < 2012100800) {
+    if ($oldversion < 2012110702) {
         // Changing precision of field name on table questionnaire_survey to (255).
 
         // First drop the index.
@@ -27,59 +27,28 @@ function xmldb_questionnaire_upgrade($oldversion) {
         $dbman->change_field_precision($table, $field);
 
         // Add back in the index.
-        $table = new xmldb_table('questionnaire_survey');
-        $index = new xmldb_index('name');
-        $index->set_attributes(XMLDB_INDEX_NOTUNIQUE, array('name'));
         $dbman->add_index($table, $index);
 
-        // questionnaire savepoint reached
-        upgrade_mod_savepoint(true, 2012100800, 'questionnaire');
-    }
-
-    if ($oldversion < 2012100800) {
-        // Changing precision of field name on table questionnaire_survey to (255).
-
-        // First drop the index.
-        $table = new xmldb_table('questionnaire_survey');
-        $index = new xmldb_index('name');
-        $index->set_attributes(XMLDB_INDEX_NOTUNIQUE, array('name'));
-        $dbman->drop_index($table, $index);
-
-        // Launch change of precision for field name.
-        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null, 'id');
-        $dbman->change_field_precision($table, $field);
-
-        // Add back in the index.
-        $table = new xmldb_table('questionnaire_survey');
-        $index = new xmldb_index('name');
-        $index->set_attributes(XMLDB_INDEX_NOTUNIQUE, array('name'));
-        $dbman->add_index($table, $index);
-
-        // questionnaire savepoint reached
-        upgrade_mod_savepoint(true, 2012100800, 'questionnaire');
-    }
-
-    if ($oldversion < 2012102101) {
         /// skip logic feature JR proof of concept
         /// Define field dependquestion to be added to questionnaire_question table
         $table = new xmldb_table('questionnaire_question');
         $field = new xmldb_field('dependquestion', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'deleted');
-        
+
         /// Conditionally launch add field
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
         }
-        
+
         $table = new xmldb_table('questionnaire_question');
         $field = new xmldb_field('dependchoice', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'dependquestion');
-        
+
         /// Conditionally launch add field
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
         }
-        
+
         // savepoint reached
-        upgrade_mod_savepoint(true, 2012102101,  'questionnaire');
+        upgrade_mod_savepoint(true, 2012110702,  'questionnaire');
     }
     return $result;
 }
